8bbf8845f6c32e309e425c458a998c3142a763bc
Mackay89 <manotomackay@gmail.com>
2024-08-21 09:15:34 -0700

    task

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
-"""LIFO Caching module.
-"""
+"""LIFO Caching system."""
+
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """LIFO Caching system.
-    """
+    """Represents an LIFO caching system."""
 
     def __init__(self):
-        """Initialize the LIFOCache object.
-        """
+        """Initialize LIFO cache."""
         super().__init__()
 
     def put(self, key, item):
-        """Add an item to the cache.
-        """
+        """Add an item to the cache."""
         if key is None or item is None:
             return
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            # Discard the last item put in cache
-            discarded_key = next(reversed(self.cache_data))
-            del self.cache_data[discarded_key]
-            print(f"DISCARD: {discarded_key}")
-        self.cache_data[key] = item
+        if key in self.cache_data:
+            # Existing key, update value
+            self.cache_data[key] = item
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Cache full, remove last item
+                last_key = list(self.cache_data.keys())[-1]
+                del self.cache_data[last_key]
+                print(f"DISCARD: {last_key}")
+            self.cache_data[key] = item
 
     def get(self, key):
-        """Retrieve an item by key.
-        """
+        """Retrieve an item from the cache."""
         return self.cache_data.get(key, None)
