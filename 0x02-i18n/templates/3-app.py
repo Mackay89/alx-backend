9c7788cf4d1a06eee73bbe51f09ee97b4eab2ce9
Mackay89 <manotomackay@gmail.com>
2024-08-27 07:49:43 -0700

    task

---
+++ 0x02-i18n/templates/3-app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+babel = Babel(app)
+
+@app.route('/')
+def home():
+    return render_template('3-index.html')
+
+if __name__ == '__main__':
+    app.run()
+
